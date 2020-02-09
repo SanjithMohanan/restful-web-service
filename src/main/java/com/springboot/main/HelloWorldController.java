268e9fd057edbcbcba0d540b4a91b7586982a847
SanjithMohanan <sanjith.mohanan@gmail.com>
2020-02-09 14:44:53 -0600

    first commit

---
+++ src/main/java/com/springboot/main/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.springboot.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller  
+@RestController
+public class HelloWorldController {
+//using get method and hello-world as URI  
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World"); // constructor of HelloWorldBean
+	}
+
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name)); // %s replace the name
+	}
+}
